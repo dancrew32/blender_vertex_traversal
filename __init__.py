e52ffe250688c549ba6a8adce939eea8206cb8b3
Dan Masquelier <dancrew32@gmail.com>
2022-01-13 21:36:11 -0800

    Blender vertex traversal addon

---
+++ __init__.py
@@ -0,0 +1,130 @@
+import bpy
+
+
+bl_info = {
+    "name": "Vertex Traversal",
+    "blender": (3, 0, 0),
+    "category": "3D View",
+}
+
+
+def set_object_mode():
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+
+def set_edit_mode():
+    bpy.ops.object.mode_set(mode='EDIT')
+
+
+def deselect():
+    bpy.ops.mesh.select_all(action='DESELECT')
+
+
+def get_active_object():
+    return bpy.context.active_object
+
+
+def get_vertex_data():
+    set_object_mode()
+    obj = get_active_object()
+    set_edit_mode()
+    bpy.ops.mesh.select_mode(type='VERT')
+    current_vertex_index = 0
+    vertices_length = len(obj.data.vertices)
+    for key, value in obj.data.vertices.items():
+        if value.select:
+            current_vertex_index = key   
+    return {'current_vertex_index': current_vertex_index,
+            'vertices_length': vertices_length}
+
+
+def get_next_vertex():
+    data = get_vertex_data()
+    return 0 if data['current_vertex_index'] + 1 >= data['vertices_length'] else data['current_vertex_index'] + 1
+
+
+def get_prev_vertex():
+    data = get_vertex_data()
+    return data['vertices_length'] - 1 if data['current_vertex_index'] - 1 < 0 else data['current_vertex_index'] - 1
+
+
+def select_next_vertex():
+    obj = get_active_object()
+    deselect()
+    set_object_mode()
+    next_vertex_index = get_next_vertex()
+    obj.data.vertices[next_vertex_index].select = True
+    set_edit_mode()
+  
+
+def select_prev_vertex():
+    obj = get_active_object()
+    deselect()
+    set_object_mode()
+    prev_vertex_index = get_prev_vertex()
+    obj.data.vertices[prev_vertex_index].select = True
+    set_edit_mode()
+
+
+# Select Next Vertex:
+select_next_vertex()
+
+# Or Select Prev Vertex
+select_prev_vertex()
+
+
+class Panel(bpy.types.Panel):
+    """Show Vertex Traversal side panel in 3D View"""
+
+    bl_idname = "VERTEX_TRAVERSAL_PT_PANEL"
+    bl_label = "Vertex Traversal"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Vertex Traversal"
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode in {'EDIT'}
+
+    def draw(self, context):
+        layout = self.layout
+        box = layout.box()
+        row = box.row()
+        row.operator("vertex.prev")
+        row.operator("vertex.next")
+
+
+
+class OpNextVertex(bpy.types.Operator):
+    bl_idname = 'vertex.next'
+    bl_label = 'Next Vertex'
+
+    def execute(self, context):
+        select_next_vertex()
+        return {'FINISHED'}
+
+
+class OpPrevVertex(bpy.types.Operator):
+    bl_idname = 'vertex.prev'
+    bl_label = 'Prev Vertex'
+
+    def execute(self, context):
+        select_prev_vertex()
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(OpNextVertex)
+    bpy.utils.register_class(OpPrevVertex)
+    bpy.utils.register_class(Panel)
+
+
+def unregister():
+    bpy.utils.unregister_class(OpNextVertex)
+    bpy.utils.unregister_class(OpPrevVertex)
+    bpy.utils.unregister_class(Panel)
+
+
+if __name__ == "__main__":
+    register()
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Blender Vertex Traversal
+
+I asked this on the blender stack exchange and decided to try building it myself:
+https://blender.stackexchange.com/q/249709/70346
+
+Sometimes you are working with a string or path and you want to conveniently select the next or previous vertex. This plugin works by picking the next or previous available vertex in a mesh during edit mode.
+
+## Requirements
+Blender 3.0.0+ (Tested in LTS)
+
+## Installation
+1. Download Latest Release https://github.com/dancrew32/blender_vertex_traversal/archive/refs/tags/v1_0.zip
+2. Edit > Preferences > Add-ons > Install > blender_vertex_traversal-1_0.zip
+3. Enable "Vertex Traversal" add-on.
+4. Observe "Vertex Traversal" panel in 3D View during Edit Mode (see usage).
+
+## Usage
+
+1. Select an object and change to edit mode.
+2. Open the "Vertex Traversal" panel
+3. Select Next or Prev to cycle through the vertices (aka `vertex.prev` or `vertex.next`)
+
